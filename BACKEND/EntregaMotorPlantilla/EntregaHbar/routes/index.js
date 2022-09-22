d45cb3230c287c176f79b5e40b201f51a75d32c3
gslopezma <64046938+gslopezma@users.noreply.github.com>
2022-09-22 01:32:28 -0300

    Update index.js

--- BACKEND/EntregaMotorPlantilla/EntregaHbar/routes/index.js
+++ BACKEND/EntregaMotorPlantilla/EntregaHbar/routes/index.js
@@ -20,22 +20,7 @@ router.get('/', async (req, res) => {
         res.status(400).json(error)
     }
 })
-/*
-router.get('/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        await readProductFile()
-        const idObj = productsArray.find((producto) => producto.id == id)
-        if (idObj == null) {
-            res.send({ error: `No existe un producto cuyo id sea ${id}` })
-        } else {
-            res.send(idObj)
-        }
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-*/
+
 router.post('/', async (req, res) => {
     const { body } = req
     try {
@@ -50,40 +35,5 @@ router.post('/', async (req, res) => {
         res.status(400).json(error)
     }
 })
-/*
-router.put('/:id', dataValidation, async (req, res) => {
-    const { id } = req.params
-    const { nombre, precio, stock } = req.body
-    try {
-        await readProductFile()
-        const prods = productsArray.find((producto) => producto.id == id)
-        if (prods == null) {
-            res.send({ error: `No existe un producto cuyo id sea ${id}` })
-        } else {
-            console.log(prods)
-            prods.nombre = nombre
-            prods.precio = precio
-            prods.stock = stock
-            await fs.promises.writeFile('products.txt', JSON.stringify(productsArray))
-            res.status(200).json({ mensaje: 'Producto editado con exito', productsArray })
-        }
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
 
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        await readProductFile()
-        console.log(1)
-        const filter = productsArray.filter(num => Number(num.id) !== Number(id))
-        console.log(1)
-        await fs.promises.writeFile('products.txt', JSON.stringify(filter))
-        res.status(200).json({ mensaje: 'Producto eliminado con exito', filter })
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-*/
 module.exports = router
