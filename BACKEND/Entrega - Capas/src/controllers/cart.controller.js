f271b3978c12c0549b72e2fc4bd6aa263afcec2d
gslopezma <64046938+gslopezma@users.noreply.github.com>
2023-01-11 14:57:18 -0300

    Add files via upload

---
+++ BACKEND/Entrega - Capas/src/controllers/cart.controller.js
@@ -0,0 +1,53 @@
+import { CarritoService } from "../services/carrito.service.js"
+import { ProductoService } from "../services/producto.service.js"
+
+const carritoService = new CarritoService()
+
+export async function create(req, res) {
+    const newCart = await carritoService.createCart()
+
+    newCart
+        ? res.status(200).json({ "success": "Carro añadido" })
+        : res.status(500).json({ "error": "No se pudo crear el carro" })
+}
+
+export async function remove(req, res) {
+    const { id } = req.params
+    const wasDeleted = await carritoService.deleteCartById(id)
+
+    wasDeleted
+        ? res.status(200).json({ "success": "Carro eliminado" })
+        : res.status(404).json({ "error": "Carro no encontrado" })
+}
+
+export async function addProduct(req, res) {
+    const { id } = req.params
+    const { body } = req
+
+    const productExists = await ProductoService.exists(body.productId)
+
+    if (productExists) {
+        await carritoService.saveProductToCart(id, body)
+    } else {
+        res.status(404).json({ "error": "Producto no encontrado" })
+    }
+}
+
+export async function getProducts(req, res) {
+    const { id } = req.params
+    const cartProducts = await carritoService.getAllProductsFromCart(id)
+
+    cartProducts
+        ? res.status(200).json(cartProducts)
+        : res.status(404).json({ "error": "Carro no encontrado" })
+}
+
+export async function removeProduct(req, res) {
+    const { id, id_prod } = req.params
+
+    const wasDeleted = await carritoService.deleteProductFromCart(id, id_prod)
+
+    wasDeleted
+        ? res.status(200).json({ "success": "Producto eliminado del carro" })
+        : res.status(400).json({ "error": "No se pudo eliminar el producto del carro. Verificar datos" })
+}
---
+++ BACKEND/Entrega - Capas/src/controllers/product.controller.js
@@ -0,0 +1,47 @@
+import { ProductoService } from "../services/producto.service.js"
+
+const productoService = new ProductoService()
+
+export async function getAll(req, res) {
+    const products = await productoService.getAll()
+    products
+        ? res.status(200).json(products)
+        : res.status(400).json({ "error": "No se pudo listar los productos" })
+}
+
+export async function getById(req, res) {
+    const { id } = req.params
+    const product = await productoService.getProductById(id)
+
+    product
+        ? res.status(200).json(product)
+        : res.status(400).json({ "error": "Producto no encontrado" })
+}
+
+export async function create(req, res) {
+    const { body } = req
+    const newProduct = await productoService.createProduct(body)
+
+    newProduct
+        ? res.status(200).json({ "success": "Producto añadido" })
+        : res.status(400).json({ "error": "No se pudo añadir el producto. Verificar datos" })
+}
+
+export async function update(req, res) {
+    const { id } = req.params
+    const { body } = req
+    const wasUpdated = await productoService.updateProductById(id, body)
+
+    wasUpdated
+        ? res.status(200).json({ "success": "Producto actualizado" })
+        : res.status(404).json({ "error": "Producto no encontrado" })
+}
+
+export async function remove(req, res) {
+    const { id } = req.params
+    const wasDeleted = await productoService.deleteProductById(id)
+
+    wasDeleted
+        ? res.status(200).json({ "success": "Producto removido" })
+        : res.status(404).json({ "error": "Producto no encontrado" })
+}
