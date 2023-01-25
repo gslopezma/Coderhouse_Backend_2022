2309c16946cd818b85336c18ed7366e8b6e46ded
gslopezma <64046938+gslopezma@users.noreply.github.com>
2023-01-25 19:43:15 -0300

    Add files via upload

---
+++ BACKEND/Entrega - Arquitectura/src/configs/db.config.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import logger from '../utils/loggers/Log4jsLogger.js'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI, (err) => {
+    err
+        ? logger.log("Error al conectarse a MongoDB")
+        : logger.log("Conectados a MongoDB")
+})
+
+export default mongoose
+
