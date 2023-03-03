f138cc4a06d94f2e03b5c36fe1320c5c8ccefac3
gslopezma <64046938+gslopezma@users.noreply.github.com>
2023-03-03 15:14:41 -0300

    Add files via upload

---
+++ BACKEND/Entrega - Final 4/src/services/BaseDao.js
@@ -0,0 +1,25 @@
+import logger from "../utils/loggers/Log4jsLogger.js"
+import "../configs/db.config.js"
+
+/** @Abstract */
+export class BaseDao {
+
+    constructor() {
+        this.logger = logger
+        if (this.constructor === BaseDao) {
+            throw new Error('Clase "BaseDao" no puedo ser instanciada')
+        }
+    }
+
+    create() {
+        throw new Error('Metodo create() debe estar implementado')
+    }
+
+    getAll() {
+        throw new Error('Metodo getAll() debe estar implementado')
+    }
+
+    deleteById() {
+        throw new Error('Metodo deleteById() debe estar implementado')
+    }
+}
